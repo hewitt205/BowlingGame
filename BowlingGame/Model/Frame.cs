5ac981099527851d92d4ec7a9ffaffe423345e2f
hewit <hewit@DESKTOP-0JL9VMU>
2021-08-30 22:26:53 -0500

    Add project files.

---
+++ BowlingGame/Model/Frame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingGame.Model
+{
+    public class Frame
+    {
+        public int Id { get; set; }
+        public int Roll1 { get; set; }
+        public int Roll2 { get; set; }
+        public int Roll3 { get; set; }
+    }
+}
