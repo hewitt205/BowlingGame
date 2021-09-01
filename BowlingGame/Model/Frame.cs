63860b193673726fff90fe59953802b8e9d7dee0
Brandon Hewitt <hewitt205@yahoo.com>
2021-08-31 22:02:58 -0500

    updates to finish the game

--- BowlingGame/Model/Frame.cs
+++ BowlingGame/Model/Frame.cs
@@ -8,8 +8,14 @@ namespace BowlingGame.Model
     public class Frame
     {
         public int Id { get; set; }
+        public int FrameScore { get; set; }
+        public bool Strike { get; set; }
+        public bool Spare { get; set; }
         public int Roll1 { get; set; }
         public int Roll2 { get; set; }
         public int Roll3 { get; set; }
+        public bool IsTask1 { get; set; }
+        public bool IsTask2 { get; set; }
+        public bool IsTask3 { get; set; }
     }
 }
